fce4544d5e92ae81667448f87625883ac4b56a76
Gabriel Escalona <gabolore@gmail.com>
2025-06-17 18:40:35 -0400

    Add interface for ColumnName Constants and return null instead of ""

---
+++ pos/src/main/java/org/spin/pos/util/IGiftCard.java
@@ -0,0 +1,11 @@
+package org.spin.pos.util;
+
+public interface IGiftCard {
+    String GIFT_CARD_TABLE_NAME = "ECA14_GiftCard";
+    String GIFT_CARD_LINE_TABLE_NAME = "ECA14_GiftCardLine";
+    String COLUMNNAME_IsGenerateGiftCard = "IsGenerateGiftCard";
+    String COLUMNNAME_IsPrepayment = "IsPrepayment";
+    String COLUMNNAME_DateDoc = "DateDoc";
+    String COLUMNNAME_Amount = "Amount";
+    String COLUMNNAME_ECA14_GiftCard_ID = "ECA14_GiftCard_ID";
+}
