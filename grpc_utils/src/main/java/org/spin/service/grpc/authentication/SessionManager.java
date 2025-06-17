c9dcb7ff4e03871a2377c9ad9cecb6c4034de6b6
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2025-06-17 12:23:37 -0400

    fix: User login load preferences on first time. (#192)

--- grpc_utils/src/main/java/org/spin/service/grpc/authentication/SessionManager.java
+++ grpc_utils/src/main/java/org/spin/service/grpc/authentication/SessionManager.java
@@ -60,6 +60,7 @@ import org.compiere.util.Util;
 import org.spin.eca52.util.JWTUtil;
 import org.spin.model.MADToken;
 import org.spin.model.MADTokenDefinition;
+import org.spin.service.grpc.util.base.PreferenceUtil;
 import org.spin.service.grpc.util.value.NumberManager;
 import org.spin.util.IThirdPartyAccessGenerator;
 import org.spin.util.ITokenGenerator;
@@ -339,6 +340,12 @@ public class SessionManager {
 		} else {
 			bearerToken = createAndGetBearerToken(session, warehouseId, Env.getAD_Language(session.getCtx()));
 		}
+
+		// Update session preferences
+		PreferenceUtil.saveSessionPreferences(
+			userId, language, session.getAD_Role_ID(), session.getAD_Client_ID(), session.getAD_Org_ID(), warehouseId
+		);
+
 		return bearerToken;
 	}
 
@@ -607,19 +614,6 @@ public class SessionManager {
 		//	Load preferences
 		loadPreferences(context);
 	}
-	
-	/**
-	 * Get Default Warehouse after login
-	 * @param organizationId
-	 * @return
-	 */
-	public static int getDefaultWarehouseId(int organizationId) {
-		if (organizationId < 0) {
-			return -1;
-		}
-		String sql = "SELECT M_Warehouse_ID FROM M_Warehouse WHERE IsActive = 'Y' AND AD_Org_ID = ? AND IsInTransit='N'";
-		return DB.getSQLValue(null, sql, organizationId);
-	}
 
 	/**
 	 * Get Default role after login
@@ -662,8 +656,21 @@ public class SessionManager {
 				+ "OR (r.IsUseUserOrgAccess='N' AND EXISTS(SELECT 1 FROM AD_Role_OrgAccess ra WHERE ra.AD_Org_ID = o.AD_Org_ID AND ra.AD_Role_ID = r.AD_Role_ID AND ra.IsActive='Y')) "
 				+ "OR (r.IsUseUserOrgAccess='Y' AND EXISTS(SELECT 1 FROM AD_User_OrgAccess ua WHERE ua.AD_Org_ID = o.AD_Org_ID AND ua.AD_User_ID = ? AND ua.IsActive='Y'))"
 				+ ") "
-			+ "ORDER BY o.AD_Org_ID DESC, o.Name";
+			+ "ORDER BY o.Name";
 		return DB.getSQLValue(null, organizationSQL, roleId, userId);
+	}
+	
+	/**
+	 * Get Default Warehouse after login
+	 * @param organizationId
+	 * @return
+	 */
+	public static int getDefaultWarehouseId(int organizationId) {
+		if (organizationId < 0) {
+			return -1;
+		}
+		String sql = "SELECT M_Warehouse_ID FROM M_Warehouse WHERE IsActive = 'Y' AND AD_Org_ID = ? AND IsInTransit='N' ORDER BY Name";
+		return DB.getSQLValue(null, sql, organizationId);
 	}
 
 	/**
