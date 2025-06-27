88e77c2502d0ca487254b1877b33475021f7a37c
yamelsenih <ysenih@erpya.com>
2025-06-27 16:20:53 -0400

    Fixes: https://github.com/solop-develop/adempiere-base/issues/221

--- base/src/org/compiere/model/MPaymentProcessor.java
+++ base/src/org/compiere/model/MPaymentProcessor.java
@@ -21,9 +21,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 import java.util.logging.Level;
 
+import org.adempiere.core.domains.models.I_C_PaymentProcessor;
 import org.adempiere.core.domains.models.X_C_PaymentProcessor;
 import org.compiere.util.CLogger;
 import org.compiere.util.DB;
@@ -36,96 +38,66 @@ import org.compiere.util.Env;
  *  @author Jorg Janke
  *  @version $Id: MPaymentProcessor.java,v 1.3 2006/07/30 00:51:03 jjanke Exp $
  */
-public class MPaymentProcessor extends X_C_PaymentProcessor
-{
+public class MPaymentProcessor extends X_C_PaymentProcessor {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1825454310856682804L;
-
-	public static MPaymentProcessor[] find (Properties ctx,
-			String tender, String CCType,
-			int AD_Client_ID, int AD_Org_ID, int C_Currency_ID, BigDecimal Amt, String trxName)
-		{
-			return find(ctx, tender, CCType, AD_Client_ID, C_Currency_ID, Amt, trxName);
-		}
 	
 	/**
 	 * 	Get BankAccount & PaymentProcessor
-	 * 	@param ctx context
 	 *  @param tender optional Tender see TENDER_
-	 *  @param CCType optional CC Type see CC_
-	 *  @param AD_Client_ID Client
-	 *  @param C_Currency_ID Currency (ignored)
-	 *  @param Amt Amount (ignored)
-	 *	@param trxName transaction
+	 *  @param creditCardCType optional CC Type see CC_
+	 *  @param currencyId Currency (ignored)
+	 *  @param paymentAmount Amount (ignored)
 	 *  @return Array of BankAccount[0] & PaymentProcessor[1] or null
 	 */
-	protected static MPaymentProcessor[] find (Properties ctx,
-		String tender, String CCType,
-		int AD_Client_ID, int C_Currency_ID, BigDecimal Amt, String trxName)
-	{
-		ArrayList<MPaymentProcessor> list = new ArrayList<MPaymentProcessor>();
-
+	protected static List<MPaymentProcessor> find (String tender, String creditCardCType, int currencyId, BigDecimal paymentAmount) {
 		if (MPayment.TENDERTYPE_Cash.equals(tender)) {
 			return null;
 		}
-		StringBuffer sql = new StringBuffer("SELECT * "
-			+ "FROM C_PaymentProcessor "
-			+ "WHERE AD_Client_ID=? AND IsActive='Y'"				//	#1
-			+ " AND (C_Currency_ID IS NULL OR C_Currency_ID=?)"		//	#2
-			+ " AND (MinimumAmt IS NULL OR MinimumAmt = 0 OR MinimumAmt <= ?)");	//	#3
-		if (MPayment.TENDERTYPE_DirectDeposit.equals(tender))
-			sql.append(" AND AcceptDirectDeposit='Y'");
-		else if (MPayment.TENDERTYPE_DirectDebit.equals(tender))
-			sql.append(" AND AcceptDirectDebit='Y'");
-		else if (MPayment.TENDERTYPE_Check.equals(tender))
-			sql.append(" AND AcceptCheck='Y'");
-		//  CreditCards
-		else if (MPayment.CREDITCARDTYPE_ATM.equals(CCType))
-			sql.append(" AND AcceptATM='Y'");
-		else if (MPayment.CREDITCARDTYPE_Amex.equals(CCType))
-			sql.append(" AND AcceptAMEX='Y'");
-		else if (MPayment.CREDITCARDTYPE_Visa.equals(CCType))
-			sql.append(" AND AcceptVISA='Y'");
-		else if (MPayment.CREDITCARDTYPE_MasterCard.equals(CCType))
-			sql.append(" AND AcceptMC='Y'");
-		else if (MPayment.CREDITCARDTYPE_Diners.equals(CCType))
-			sql.append(" AND AcceptDiners='Y'");
-		else if (MPayment.CREDITCARDTYPE_Discover.equals(CCType))
-			sql.append(" AND AcceptDiscover='Y'");
-		else if (MPayment.CREDITCARDTYPE_PurchaseCard.equals(CCType))
-			sql.append(" AND AcceptCORPORATE='Y'");
-		//
-		try
-		{
-			PreparedStatement pstmt = DB.prepareStatement(sql.toString(), trxName);
-			pstmt.setInt(1, AD_Client_ID);
-			pstmt.setInt(2, C_Currency_ID);
-			pstmt.setBigDecimal(3, Amt);
-			ResultSet rs = pstmt.executeQuery();
-			while (rs.next())
-				list.add(new MPaymentProcessor (ctx, rs, trxName));
-			rs.close();
-			pstmt.close();
+		List<Object> parameters = new ArrayList<>();
+		StringBuilder whereClause = new StringBuilder("(C_Currency_ID IS NULL OR C_Currency_ID=?)"
+				+ " AND (MinimumAmt IS NULL OR MinimumAmt = 0 OR MinimumAmt <= ?)");
+		parameters.add(currencyId);
+		parameters.add(paymentAmount);
+		if (MPayment.TENDERTYPE_DirectDeposit.equals(tender)) {
+			whereClause.append(" AND AcceptDirectDeposit='Y'");
+		} else if (MPayment.TENDERTYPE_DirectDebit.equals(tender)) {
+			whereClause.append(" AND AcceptDirectDebit='Y'");
+		} else if (MPayment.TENDERTYPE_Check.equals(tender)) {
+			whereClause.append(" AND AcceptCheck='Y'");
+		} else if (MPayment.CREDITCARDTYPE_ATM.equals(creditCardCType)) {
+			whereClause.append(" AND AcceptATM='Y'");
+		} else if (MPayment.CREDITCARDTYPE_Amex.equals(creditCardCType)) {
+			whereClause.append(" AND AcceptAMEX='Y'");
+		} else if (MPayment.CREDITCARDTYPE_Visa.equals(creditCardCType)) {
+			whereClause.append(" AND AcceptVISA='Y'");
+		} else if (MPayment.CREDITCARDTYPE_MasterCard.equals(creditCardCType)) {
+			whereClause.append(" AND AcceptMC='Y'");
+		} else if (MPayment.CREDITCARDTYPE_Diners.equals(creditCardCType)) {
+			whereClause.append(" AND AcceptDiners='Y'");
+		} else if (MPayment.CREDITCARDTYPE_Discover.equals(creditCardCType)) {
+			whereClause.append(" AND AcceptDiscover='Y'");
+		} else if (MPayment.CREDITCARDTYPE_PurchaseCard.equals(creditCardCType)) {
+			whereClause.append(" AND AcceptCORPORATE='Y'");
 		}
-		catch (SQLException e)
-		{
-			s_log.log(Level.SEVERE, "find - " + sql, e);
-			return null;
-		}
+		List<MPaymentProcessor> list = new Query(Env.getCtx(), I_C_PaymentProcessor.Table_Name, whereClause.toString(), null)
+				.setParameters(parameters)
+				.setClient_ID()
+				.setOnlyActiveRecords(true)
+				.list();
 		//
-		if (list.size() == 0)
-			s_log.warning("find - not found - AD_Client_ID=" + AD_Client_ID
-				+ ", C_Currency_ID=" + C_Currency_ID + ", Amt=" + Amt);
-		else
-			s_log.fine("find - #" + list.size() + " - AD_Client_ID=" + AD_Client_ID
-				+ ", C_Currency_ID=" + C_Currency_ID + ", Amt=" + Amt);
-		MPaymentProcessor[] retValue = new MPaymentProcessor[list.size()];
-		list.toArray(retValue);
-		return retValue;
+		if (list.isEmpty()) {
+			s_log.warning("find - not found - AD_Client_ID=" + Env.getAD_Client_ID(Env.getCtx())
+					+ ", C_Currency_ID=" + currencyId + ", Amt=" + paymentAmount);
+		} else {
+			s_log.fine("find - #" + list.size() + " - AD_Client_ID=" + Env.getAD_Client_ID(Env.getCtx())
+					+ ", C_Currency_ID=" + currencyId + ", Amt=" + paymentAmount);
+		}
+		return list;
 	}   //  find
-	
+
 	/**	Static Logger	*/
 	private static CLogger	s_log	= CLogger.getCLogger (MPaymentProcessor.class);
 
@@ -189,24 +161,21 @@ public class MPaymentProcessor extends X_C_PaymentProcessor
 	/**
 	 * 	Does Payment Processor accepts tender / CC
 	 *	@param TenderType tender type
-	 *	@param CreditCardType credit card type
+	 *	@param creditCardType credit card type
 	 *	@return true if acceptes
 	 */
-	public boolean accepts (String TenderType, String CreditCardType)
-	{
-		if ((MPayment.TENDERTYPE_DirectDeposit.equals(TenderType) && isAcceptDirectDeposit())
-			|| (MPayment.TENDERTYPE_DirectDebit.equals(TenderType) && isAcceptDirectDebit())
-			|| (MPayment.TENDERTYPE_Check.equals(TenderType) && isAcceptCheck())
-			//
-			|| (MPayment.CREDITCARDTYPE_ATM.equals(CreditCardType) && isAcceptATM())
-			|| (MPayment.CREDITCARDTYPE_Amex.equals(CreditCardType) && isAcceptAMEX())
-			|| (MPayment.CREDITCARDTYPE_PurchaseCard.equals(CreditCardType) && isAcceptCorporate())
-			|| (MPayment.CREDITCARDTYPE_Diners.equals(CreditCardType) && isAcceptDiners())
-			|| (MPayment.CREDITCARDTYPE_Discover.equals(CreditCardType) && isAcceptDiscover())
-			|| (MPayment.CREDITCARDTYPE_MasterCard.equals(CreditCardType) && isAcceptMC())
-			|| (MPayment.CREDITCARDTYPE_Visa.equals(CreditCardType) && isAcceptVisa()))
-			return true;
-		return false;
-	}	//	accepts
+	public boolean accepts (String TenderType, String creditCardType) {
+        return (MPayment.TENDERTYPE_DirectDeposit.equals(TenderType) && isAcceptDirectDeposit())
+                || (MPayment.TENDERTYPE_DirectDebit.equals(TenderType) && isAcceptDirectDebit())
+                || (MPayment.TENDERTYPE_Check.equals(TenderType) && isAcceptCheck())
+                //
+                || (MPayment.CREDITCARDTYPE_ATM.equals(creditCardType) && isAcceptATM())
+                || (MPayment.CREDITCARDTYPE_Amex.equals(creditCardType) && isAcceptAMEX())
+                || (MPayment.CREDITCARDTYPE_PurchaseCard.equals(creditCardType) && isAcceptCorporate())
+                || (MPayment.CREDITCARDTYPE_Diners.equals(creditCardType) && isAcceptDiners())
+                || (MPayment.CREDITCARDTYPE_Discover.equals(creditCardType) && isAcceptDiscover())
+                || (MPayment.CREDITCARDTYPE_MasterCard.equals(creditCardType) && isAcceptMC())
+                || (MPayment.CREDITCARDTYPE_Visa.equals(creditCardType) && isAcceptVisa());
+    }	//	accepts
 
 }	//	MPaymentProcessor
