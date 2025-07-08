a7e3f8fbaf9c45d2dac71042119528e4299fba45
GLescalona <gabolore@gmail.com>
2025-07-08 14:58:06 -0400

    Allows to Run Commission without Commission Sales rep (#235)
    * Allows to Run Commission without Commission Sales rep
    
    * Add validation for commissions based on Sales Representative
    
    * Add XMl for Commission for Sales Rep nad fix column name validation
    
    * Add XML ReleaseNo
    
    * Delete return statement
    
    * Change ColumnName to IsCommissionForSalesReps

--- base/src/org/compiere/model/MCommissionRun.java
+++ base/src/org/compiere/model/MCommissionRun.java
@@ -288,30 +288,51 @@ public class MCommissionRun extends X_C_CommissionRun implements DocAction, DocO
 		
 		log.info("StartDate = " + getStartDate() + ", EndDate = " + getEndDate());
 		
-		//	Iterate for each commission definition and  Sales Representative
+		//	Iterate for each commission definition
 		for(MCommission commission : commissionList) {
-			for(MBPartner salesRep : commission.getSalesRepsOfCommission()) {
-				//	Add support to Commission Calculation Class
-				if(commission.getC_CommissionType_ID() > 0) {
+			if (!commission.get_ValueAsBoolean("IsCommissionForSalesReps")){
+				if (commission.getC_CommissionType_ID() > 0) {
 					MCommissionType commissionType = MCommissionType.getById(getCtx(), commission.getC_CommissionType_ID(), get_TrxName());
-					if(commissionType.get_ValueAsBoolean("IsViewBased")) {
-						processCommissionLine(salesRep, commission);
-					} else {
-						//	Load Class or Rule
+					if (!commissionType.get_ValueAsBoolean("IsViewBased")) {
 						if(!Util.isEmpty(commissionType.get_ValueAsString("Classname"))) {
 							ICommissionCalculation commissionEngine = CommissionClassLoader.loadClass(commissionType.get_ValueAsString("Classname"));
 							if(commissionEngine == null) {
 								throw new AdempiereException("@Classname@ @NotFound@");
 							}
-							commissionEngine.processCommission(getCtx(), salesRep.getC_BPartner_ID(), commission.getC_Commission_ID(), getC_CommissionRun_ID(), get_TrxName());
+							commissionEngine.processCommission(getCtx(), 0, commission.getC_Commission_ID(), getC_CommissionRun_ID(), get_TrxName());
 						} else if(commissionType.getAD_Rule_ID() > 0) {
-							
+
 						} else {
 							throw new AdempiereException("@Classname@ @NotFound@");
 						}
+						saveEx();
 					}
-				} else {
-					processCommissionLine(salesRep, commission);
+				}
+			} else {
+				//Iterate for each Sales Representative
+				for(MBPartner salesRep : commission.getSalesRepsOfCommission()) {
+					//	Add support to Commission Calculation Class
+					if(commission.getC_CommissionType_ID() > 0) {
+						MCommissionType commissionType = MCommissionType.getById(getCtx(), commission.getC_CommissionType_ID(), get_TrxName());
+						if(commissionType.get_ValueAsBoolean("IsViewBased")) {
+							processCommissionLine(salesRep, commission);
+						} else {
+							//	Load Class or Rule
+							if(!Util.isEmpty(commissionType.get_ValueAsString("Classname"))) {
+								ICommissionCalculation commissionEngine = CommissionClassLoader.loadClass(commissionType.get_ValueAsString("Classname"));
+								if(commissionEngine == null) {
+									throw new AdempiereException("@Classname@ @NotFound@");
+								}
+								commissionEngine.processCommission(getCtx(), salesRep.getC_BPartner_ID(), commission.getC_Commission_ID(), getC_CommissionRun_ID(), get_TrxName());
+							} else if(commissionType.getAD_Rule_ID() > 0) {
+
+							} else {
+								throw new AdempiereException("@Classname@ @NotFound@");
+							}
+						}
+					} else {
+						processCommissionLine(salesRep, commission);
+					}
 				}
 			}	
 		}
