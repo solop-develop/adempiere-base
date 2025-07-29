7107231a28b5a7517b262363afb631f98ca549ef
Gabriel Escalona <gabolore@gmail.com>
2025-07-29 12:35:42 -0400

    Clean code

--- resources/ddlutils/postgresql/views/RV_FO_Dispatch_Route.sql
+++ resources/ddlutils/postgresql/views/RV_FO_Dispatch_Route.sql
@@ -1,41 +1,41 @@
 CREATE OR REPLACE VIEW RV_FO_Dispatch_Route
-        AS SELECT io.AD_Client_ID,
-        io.AD_Org_ID,
-        io.IsActive,
-        o.C_BPartner_ID,
-        io.WM_InOutbound_ID,
-        p.UnitsPerPallet,
-        FLOOR(
-        CASE
-        WHEN COALESCE(p.UnitsPerPallet, 0::NUMERIC) > 0::NUMERIC THEN SUM(iol.MovementQty) / p.UnitsPerPallet
-        ELSE 0::NUMERIC
-        END) AS QtyBatchs,
-        SUM(iol.MovementQty) - FLOOR(
-        CASE
-        WHEN COALESCE(p.UnitsPerPallet, 0::NUMERIC) > 0::NUMERIC THEN SUM(iol.MovementQty) / p.UnitsPerPallet
-        ELSE 0::NUMERIC
-        END) * COALESCE(p.UnitsPerPallet, 0::NUMERIC) AS PickedQty,
-        SUM(iol.MovementQty) AS Qty,
-        SUM(iol.MovementQty * p.Weight) AS Weight,
-        SUM(iol.MovementQty * p.Volume) AS Volume,
-        iol.C_UOM_ID,
-        o.C_Order_ID,
-        COALESCE(loc.Address1, '') || COALESCE(', ' || loc.Address2, '') || COALESCE(', ' || loc.Address3, '') || COALESCE(', ' || loc.Address4, '') DirectionName,
-        loc.City,
-        i.DocumentNo,
-        iol.C_Invoice_ID,
-        i.PaymentRule,
-        i.GrandTotal,
-        NULL::TEXT AS Comments
-        FROM WM_InOutbound io
-        JOIN WM_InOutboundLine iol ON iol.WM_InOutbound_ID = io.WM_InOutbound_ID
-        JOIN c_order o ON o.c_order_id = iol.c_order_id
-        JOIN c_bpartner bp ON bp.C_BPartner_ID = o.C_BPartner_ID
-        JOIN m_product p ON p.M_Product_ID = iol.M_Product_ID
-        JOIN C_BPartner_Location bpl ON (bpl.C_BPartner_Location_ID = o.C_BPartner_Location_ID)
-        JOIN C_Location loc ON (loc.C_Location_ID = bpl.C_Location_ID)
-        LEFT JOIN C_Invoice i ON (i.C_Invoice_ID = iol.C_Invoice_ID)
-        GROUP BY io.AD_Client_ID, io.AD_Org_ID, io.IsActive, o.C_BPartner_ID, iol.C_UOM_ID, io.WM_InOutbound_ID, p.UnitsPerPallet,o.C_Order_ID, loc.Address1,
-        loc.Address2, loc.Address3, loc.Address4, loc.City, i.DocumentNo, iol.C_Invoice_ID, i.PaymentRule, i.Grandtotal
-        ORDER BY o.C_BPartner_ID
-        ;
+AS SELECT io.AD_Client_ID,
+io.AD_Org_ID,
+io.IsActive,
+o.C_BPartner_ID,
+io.WM_InOutbound_ID,
+p.UnitsPerPallet,
+FLOOR(
+CASE
+WHEN COALESCE(p.UnitsPerPallet, 0::NUMERIC) > 0::NUMERIC THEN SUM(iol.MovementQty) / p.UnitsPerPallet
+ELSE 0::NUMERIC
+END) AS QtyBatchs,
+SUM(iol.MovementQty) - FLOOR(
+CASE
+WHEN COALESCE(p.UnitsPerPallet, 0::NUMERIC) > 0::NUMERIC THEN SUM(iol.MovementQty) / p.UnitsPerPallet
+ELSE 0::NUMERIC
+END) * COALESCE(p.UnitsPerPallet, 0::NUMERIC) AS PickedQty,
+SUM(iol.MovementQty) AS Qty,
+SUM(iol.MovementQty * p.Weight) AS Weight,
+SUM(iol.MovementQty * p.Volume) AS Volume,
+iol.C_UOM_ID,
+o.C_Order_ID,
+COALESCE(loc.Address1, '') || COALESCE(', ' || loc.Address2, '') || COALESCE(', ' || loc.Address3, '') || COALESCE(', ' || loc.Address4, '') DirectionName,
+loc.City,
+i.DocumentNo,
+iol.C_Invoice_ID,
+i.PaymentRule,
+i.GrandTotal,
+NULL::TEXT AS Comments
+FROM WM_InOutbound io
+JOIN WM_InOutboundLine iol ON iol.WM_InOutbound_ID = io.WM_InOutbound_ID
+JOIN C_Order o ON o.C_Order_ID = iol.C_Order_ID
+JOIN C_BPartner bp ON bp.C_BPartner_ID = o.C_BPartner_ID
+JOIN M_Product p ON p.M_Product_ID = iol.M_Product_ID
+JOIN C_BPartner_Location bpl ON (bpl.C_BPartner_Location_ID = o.C_BPartner_Location_ID)
+JOIN C_Location loc ON (loc.C_Location_ID = bpl.C_Location_ID)
+LEFT JOIN C_Invoice i ON (i.C_Invoice_ID = iol.C_Invoice_ID)
+GROUP BY io.AD_Client_ID, io.AD_Org_ID, io.IsActive, o.C_BPartner_ID, iol.C_UOM_ID, io.WM_InOutbound_ID, p.UnitsPerPallet,o.C_Order_ID, loc.Address1,
+loc.Address2, loc.Address3, loc.Address4, loc.City, i.DocumentNo, iol.C_Invoice_ID, i.PaymentRule, i.Grandtotal
+ORDER BY o.C_BPartner_ID
+;
--- resources/ddlutils/postgresql/views/RV_FO_Partner_And_Product.sql
+++ resources/ddlutils/postgresql/views/RV_FO_Partner_And_Product.sql
@@ -1,53 +1,53 @@
 CREATE OR REPLACE VIEW RV_FO_Partner_And_Product
-    AS SELECT io.AD_Client_ID,
-    io.AD_Org_ID,
-    io.IsActive,
-    pc.M_Product_Category_ID,
-    pg.M_Product_Group_ID,
-    mpc.M_Product_Class_ID,
-    pcl.M_Product_Classification_ID,
-    isp.M_Industry_Sector_ID,
-    mg.M_Material_Group_ID,
-    mt.M_Material_Type_ID,
-    mpg.M_Purchase_Group_ID,
-    sg.M_Sales_Group_ID,
-    o.C_BPartner_ID,
-    iol.M_Product_ID,
-    io.WM_InOutbound_ID,
-    p.UnitsPerPallet,
-    FLOOR(
-    CASE
-    WHEN COALESCE(p.UnitsPerPallet, 0::NUMERIC) > 0::NUMERIC THEN SUM(iol.MovementQty) / p.UnitsPerPallet
-    ELSE 0::NUMERIC
-    END) AS QtyBatchs,
-    SUM(iol.MovementQty) - FLOOR(
-    CASE
-    WHEN COALESCE(p.UnitsPerPallet, 0::NUMERIC) > 0::NUMERIC THEN SUM(iol.MovementQty) / p.UnitsPerPallet
-    ELSE 0::NUMERIC
-    END) * COALESCE(p.UnitsPerPallet, 0::NUMERIC) AS PickedQty,
-    SUM(iol.MovementQty) AS Qty,
-    SUM(iol.MovementQty * p.Weight) AS Weight,
-    SUM(iol.MovementQty * p.Volume) AS Volume,
-    iol.C_UOM_ID,
-    o.C_Order_ID,
-    i.DocumentNo,
-    iol.C_Invoice_ID
-    FROM WM_InOutbound io
-    JOIN WM_InOutboundLine iol ON iol.WM_InOutbound_ID = io.WM_InOutbound_ID
-    JOIN c_order o ON o.c_order_id = iol.c_order_id
-    JOIN C_BPartner bp ON bp.C_BPartner_ID = o.C_BPartner_ID
-    JOIN M_Product p ON p.M_Product_ID = iol.M_Product_ID
-    JOIN M_Product_Category pc ON pc.M_Product_Category_ID = p.M_Product_Category_ID
-    LEFT JOIN M_Product_Group pg ON pg.M_Product_Group_ID = p.M_Product_Group_ID
-    LEFT JOIN M_Product_Class mpc ON mpc.M_Product_Class_ID = p.M_Product_Class_ID
-    LEFT JOIN M_Product_Classification pcl ON pcl.M_Product_Classification_ID = p.M_Product_Classification_ID
-    LEFT JOIN M_Industry_Sector isp ON isp.M_Industry_Sector_ID = p.M_Industry_Sector_ID
-    LEFT JOIN M_Material_Group mg ON mg.M_Material_Group_ID = p.M_Material_Group_ID
-    LEFT JOIN M_Material_Type mt ON mt.M_Material_Type_ID = p.M_Material_Type_ID
-    LEFT JOIN M_Purchase_Group mpg ON mpg.M_Purchase_Group_ID = p.M_Purchase_Group_ID
-    LEFT JOIN M_Sales_Group sg ON sg.M_Sales_Group_ID = p.M_Sales_Group_ID
-    JOIN C_Invoice i ON (i.C_Invoice_ID = iol.C_Invoice_ID)
-    GROUP BY io.AD_Client_ID, io.AD_Org_ID, io.IsActive, pc.M_Product_Category_ID, pg.M_Product_Group_ID, mpc.M_Product_Class_ID, pcl.M_Product_Classification_ID,
-    isp.M_Industry_Sector_ID, mg.M_Material_Group_ID, mt.M_Material_Type_ID, mpg.M_Purchase_Group_ID, sg.M_Sales_Group_ID,
-    o.C_BPartner_ID, iol.C_UOM_ID, iol.M_Product_ID, io.WM_InOutbound_ID, p.UnitsPerPallet, o.C_Order_ID, i.DocumentNo, iol.C_Invoice_ID
-    ;
+AS SELECT io.AD_Client_ID,
+io.AD_Org_ID,
+io.IsActive,
+pc.M_Product_Category_ID,
+pg.M_Product_Group_ID,
+mpc.M_Product_Class_ID,
+pcl.M_Product_Classification_ID,
+isp.M_Industry_Sector_ID,
+mg.M_Material_Group_ID,
+mt.M_Material_Type_ID,
+mpg.M_Purchase_Group_ID,
+sg.M_Sales_Group_ID,
+o.C_BPartner_ID,
+iol.M_Product_ID,
+io.WM_InOutbound_ID,
+p.UnitsPerPallet,
+FLOOR(
+CASE
+WHEN COALESCE(p.UnitsPerPallet, 0::NUMERIC) > 0::NUMERIC THEN SUM(iol.MovementQty) / p.UnitsPerPallet
+ELSE 0::NUMERIC
+END) AS QtyBatchs,
+SUM(iol.MovementQty) - FLOOR(
+CASE
+WHEN COALESCE(p.UnitsPerPallet, 0::NUMERIC) > 0::NUMERIC THEN SUM(iol.MovementQty) / p.UnitsPerPallet
+ELSE 0::NUMERIC
+END) * COALESCE(p.UnitsPerPallet, 0::NUMERIC) AS PickedQty,
+SUM(iol.MovementQty) AS Qty,
+SUM(iol.MovementQty * p.Weight) AS Weight,
+SUM(iol.MovementQty * p.Volume) AS Volume,
+iol.C_UOM_ID,
+o.C_Order_ID,
+i.DocumentNo,
+iol.C_Invoice_ID
+FROM WM_InOutbound io
+JOIN WM_InOutboundLine iol ON iol.WM_InOutbound_ID = io.WM_InOutbound_ID
+JOIN C_Order o ON o.C_Order_ID = iol.C_Order_ID
+JOIN C_BPartner bp ON bp.C_BPartner_ID = o.C_BPartner_ID
+JOIN M_Product p ON p.M_Product_ID = iol.M_Product_ID
+JOIN M_Product_Category pc ON pc.M_Product_Category_ID = p.M_Product_Category_ID
+LEFT JOIN M_Product_Group pg ON pg.M_Product_Group_ID = p.M_Product_Group_ID
+LEFT JOIN M_Product_Class mpc ON mpc.M_Product_Class_ID = p.M_Product_Class_ID
+LEFT JOIN M_Product_Classification pcl ON pcl.M_Product_Classification_ID = p.M_Product_Classification_ID
+LEFT JOIN M_Industry_Sector isp ON isp.M_Industry_Sector_ID = p.M_Industry_Sector_ID
+LEFT JOIN M_Material_Group mg ON mg.M_Material_Group_ID = p.M_Material_Group_ID
+LEFT JOIN M_Material_Type mt ON mt.M_Material_Type_ID = p.M_Material_Type_ID
+LEFT JOIN M_Purchase_Group mpg ON mpg.M_Purchase_Group_ID = p.M_Purchase_Group_ID
+LEFT JOIN M_Sales_Group sg ON sg.M_Sales_Group_ID = p.M_Sales_Group_ID
+JOIN C_Invoice i ON (i.C_Invoice_ID = iol.C_Invoice_ID)
+GROUP BY io.AD_Client_ID, io.AD_Org_ID, io.IsActive, pc.M_Product_Category_ID, pg.M_Product_Group_ID, mpc.M_Product_Class_ID, pcl.M_Product_Classification_ID,
+isp.M_Industry_Sector_ID, mg.M_Material_Group_ID, mt.M_Material_Type_ID, mpg.M_Purchase_Group_ID, sg.M_Sales_Group_ID,
+o.C_BPartner_ID, iol.C_UOM_ID, iol.M_Product_ID, io.WM_InOutbound_ID, p.UnitsPerPallet, o.C_Order_ID, i.DocumentNo, iol.C_Invoice_ID
+;
