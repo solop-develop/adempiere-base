0f6115d3eca38d83069a30ff4aa1a3bf5de04206
GLescalona <gabolore@gmail.com>
2025-09-22 18:24:51 -0400

    Generate Shipment From Order Line Allow DropShip Orders (#370)
    * Generate Shipment From Order Line Allow DropShip Orders
    
    * Fix view SQL
    
    * Fix drop view

--- resources/ddlutils/oracle/views/M_INOUT_CANDIDATEDETAIL_V.sql
+++ resources/ddlutils/oracle/views/M_INOUT_CANDIDATEDETAIL_V.sql
@@ -16,12 +16,16 @@ qtytodeliver,
 c_orderline_id,
 datepromised,
 m_product_id,
+M_Product_Group_ID,
+M_Product_Class_ID,
+M_Product_Classification_ID,
 c_charge_id,
 qtyordered,
 isunconfirmedinout,
 unconfirmedLinesNoToShip,
 deliveryrule,
-qtyavailable
+qtyavailable,
+DropShip_BPartner_ID
 )
 AS
 SELECT o.ad_client_id,
@@ -40,6 +44,9 @@ l.qtyordered - l.qtydelivered AS qtytodeliver,
 l.c_orderline_id,
 l.datepromised,
 l.m_product_id,
+prod.M_Product_Group_ID,
+prod.M_Product_Class_ID,
+prod.M_Product_Classification_ID,
 l.c_charge_id,
 l.qtyordered,
 CASE
@@ -48,9 +55,11 @@ ELSE 'Y'
 END AS isunconfirmedinout,
 iol.unconfirmedLinesNoToShip,
 o.deliveryrule,
-storage.qtyavailable - l.qtyreserved AS qtyavailable
+storage.qtyavailable - l.qtyreserved AS qtyavailable,
+o.DropShip_BPartner_ID
 FROM c_order o
 JOIN c_orderline l ON o.c_order_id = l.c_order_id
+JOIN M_Product prod ON l.M_Product_ID = prod.M_Product_ID
 LEFT JOIN ( SELECT count(iol_1.m_inoutline_id) AS unconfirmedLinesNoToShip,
 iol_1.c_orderline_id
 FROM m_inoutline iol_1
@@ -66,7 +75,7 @@ WHERE o.docstatus = 'CO' AND o.isdelivered = 'N' AND (o.c_doctype_id IN ( SELECT
 FROM c_doctype
 WHERE c_doctype.docbasetype = 'SOO' AND (c_doctype.docsubtypeso not in ('ON','OB' ,'WR')  ))) AND o.deliveryrule <> 'M' AND (l.m_product_id IS NULL OR (EXISTS ( SELECT p.value
 FROM m_product p
-WHERE l.m_product_id = p.m_product_id AND p.isexcludeautodelivery = 'N'))) AND l.qtyordered <> l.qtydelivered AND o.isdropship = 'N' AND (l.m_product_id IS NOT NULL OR l.c_charge_id IS NOT NULL) AND NOT (EXISTS ( SELECT iol_1.m_inoutline_id
+WHERE l.m_product_id = p.m_product_id AND p.isexcludeautodelivery = 'N'))) AND l.qtyordered <> l.qtydelivered AND (l.m_product_id IS NOT NULL OR l.c_charge_id IS NOT NULL) AND NOT (EXISTS ( SELECT iol_1.m_inoutline_id
 FROM m_inoutline iol_1
 JOIN m_inout io ON iol_1.m_inout_id = io.m_inout_id
 WHERE iol_1.c_orderline_id = l.c_orderline_id AND (io.docstatus  in ('IP','WC' ))));
