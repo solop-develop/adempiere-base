873d5cd2fed5b3903d71ebe8dfd7591f521c26fe
GLescalona <gabolore@gmail.com>
2025-12-11 12:12:44 -0400

    Reduce Search time for Generate Consigned Invoice (#483)
    * Reduce Search time for Generate Consigned Invoice
    
    * Add Util class for Sql Insert get Next ID

--- base/src/main/java/org/solop/sp016/process/util/ConsignmentOrderGrouping.java
+++ base/src/main/java/org/solop/sp016/process/util/ConsignmentOrderGrouping.java
@@ -1,17 +1,21 @@
 package org.solop.sp016.process.util;
 
 import java.math.BigDecimal;
+import java.sql.Timestamp;
 
 public class ConsignmentOrderGrouping {
     BigDecimal maxAmount;
     BigDecimal usedAmount;
-
-
-
     BigDecimal deliveredAmount;
     int orderLineId;
+    int clientSalesOrderLineId;
+    int inventoryLineId;
     int orderId;
+    int businessPartnerId;
+    Timestamp dateDoc;
+    int orgId;
 
+    int productId;
     public ConsignmentOrderGrouping(BigDecimal maxAmount, int orderLineId, BigDecimal deliveredAmount) {
         this.maxAmount = maxAmount;
         this.orderLineId = orderLineId;
@@ -53,4 +57,46 @@ public class ConsignmentOrderGrouping {
     public BigDecimal getDeliveredAmount() { return deliveredAmount; }
 
     public void setDeliveredAmount(BigDecimal deliveredAmount) { this.deliveredAmount = deliveredAmount; }
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+    public Timestamp getDateDoc() {
+        return dateDoc;
+    }
+
+    public void setDateDoc(Timestamp dateDoc) {
+        this.dateDoc = dateDoc;
+    }
+    public int getBusinessPartnerId() {
+        return businessPartnerId;
+    }
+
+    public void setBusinessPartnerId(int businessPartnerId) {
+        this.businessPartnerId = businessPartnerId;
+    }
+    public int getOrgId() {
+        return orgId;
+    }
+
+    public void setOrgId(int orgId) {
+        this.orgId = orgId;
+    }
+    public int getInventoryLineId() {
+        return inventoryLineId;
+    }
+
+    public void setInventoryLineId(int inventoryLineId) {
+        this.inventoryLineId = inventoryLineId;
+    }
+    public int getClientSalesOrderLineId() {
+        return clientSalesOrderLineId;
+    }
+
+    public void setClientSalesOrderLineId(int clientSalesOrderLineId) {
+        this.clientSalesOrderLineId = clientSalesOrderLineId;
+    }
 }
