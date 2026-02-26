26d2f05a7517ed33a42b40a11e552e36cc91256c
yamelsenih <ysenih@erpya.com>
2026-02-25 21:41:39 -0400

    Remove error with transaction for queue

--- base/src/main/java/org/spin/queue/util/QueueManager.java
+++ base/src/main/java/org/spin/queue/util/QueueManager.java
@@ -281,7 +281,7 @@ public abstract class QueueManager {
 				queueToProcess.setErrorMsg(error);
 			}
 			queueToProcess.setProcessed(!isError || queueType.isIgnoreError());
-			queueToProcess.saveEx(null);
+			queueToProcess.saveEx();
 		}
 		if(isError && !queueType.isIgnoreError()) {
 			throw new AdempiereException(error);
