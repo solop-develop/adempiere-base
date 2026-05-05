64ce43eb372e6a43a5dd4da5abf9c33c972a748f
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2026-05-05 11:10:35 -0400

    fix: Reference with correct context columns (#589)
    When you have a table reference, and/or a dynamic validation that uses context, it does not correctly generate the context columns.

--- kafka/src/main/java/org/spin/eca56/util/support/documents/ReferenceValues.java
+++ kafka/src/main/java/org/spin/eca56/util/support/documents/ReferenceValues.java
@@ -17,35 +17,97 @@
  *****************************************************************************/
 package org.spin.eca56.util.support.documents;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.compiere.model.MLookupInfo;
+import org.compiere.model.MTable;
+import org.compiere.util.Env;
+
 /**
  * 	The Stub class for reference
  * 	@author Yamel Senih, ysenih@erpya.com, ERPCyA http://www.erpya.com
  */
 public class ReferenceValues {
+
 	private String tableName;
+	private String accessLevel;
+	private int displayTypeId;
+	private int referenceValueId;
 	private String embeddedContextColumn;
-	private int referenceId;
-	
-	private ReferenceValues(int referenceId, String tableName, String embeddedContextColumn) {
-		this.referenceId = referenceId;
+	private List<String> contextColumns;
+
+
+
+	private ReferenceValues(String tableName, String accessLevel, int displayTypeId, int referenceValueId, String embeddedContextColumn, List<String> contextColumns) {
+		this.displayTypeId = displayTypeId;
 		this.tableName = tableName;
+		this.accessLevel = accessLevel;
+		this.referenceValueId = referenceValueId;
 		this.embeddedContextColumn = embeddedContextColumn;
+		this.contextColumns = contextColumns;
 	}
-	
-	public static ReferenceValues newInstance(int referenceId, String tableName, String embeddedContextColumn) {
-		return new ReferenceValues(referenceId, tableName, embeddedContextColumn);
+
+
+
+	public static ReferenceValues newInstance(int displayTypeId, String tableName, String embeddedContextColumn) {
+		MTable table = MTable.get(Env.getCtx(), tableName);
+		List<String> contextColumnsList = ReferenceUtil.getContextColumnNames(
+			Optional.ofNullable(embeddedContextColumn).orElse("")
+		);
+		return new ReferenceValues(
+			tableName,
+			table.getAccessLevel(),
+			displayTypeId,
+			0,
+			embeddedContextColumn,
+			contextColumnsList
+		);
 	}
-	
+
+	public static ReferenceValues newInstance(MLookupInfo lookupInfo) {
+		MTable table = MTable.get(Env.getCtx(), lookupInfo.TableName);
+		String embeddedContextColumn = Optional.ofNullable(lookupInfo.QueryDirect).orElse("")
+			+ Optional.ofNullable(lookupInfo.Query).orElse("")
+			+ Optional.ofNullable(lookupInfo.ValidationCode).orElse("")
+		;
+		List<String> contextColumnsList = ReferenceUtil.getContextColumnNames(
+			Optional.ofNullable(embeddedContextColumn).orElse("")
+		);
+		return new ReferenceValues(
+			table.getTableName(),
+			table.getAccessLevel(),
+			lookupInfo.DisplayType,
+			lookupInfo.AD_Reference_Value_ID,
+			embeddedContextColumn,
+			contextColumnsList
+		);
+	}
+
+
+
 	public String getTableName() {
 		return tableName;
 	}
-	
+
+	public String getAccessLevel() {
+		return accessLevel;
+	}
+
+	public int getDisplayTypeId() {
+		return displayTypeId;
+	}
+
+	public int getReferenceValueId() {
+		return referenceValueId;
+	}
+
 	public String getEmbeddedContextColumn() {
 		return embeddedContextColumn;
 	}
-	
-	public int getReferenceId() {
-		return referenceId;
+
+	public List<String> getContextColumns() {
+		return contextColumns;
 	}
 
 }
